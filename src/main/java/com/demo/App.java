81e0b3cfa6b605931c8ba8ea5585374fe7ee9486
Syamala Joyce Dadisetti <joycedadiseti@gmail.com>
2026-03-19 17:16:47 +0530

    Fix App.java merge conflict

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
 package com.demo;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
-=======
-package com.demo;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
->>>>>>> 58a0b8ae68e2de09382f9fe88ec37e871b117a79
